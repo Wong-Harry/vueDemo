53f1f6fc7a9468cf71a0b0106a2cc8dc8030bb9f
宏智 <7796101035@qq.com>
2020-03-04 22:28:51 +0800

    first rp

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+// 注册elementui
+Vue.use(ElementUI);
+console.log(process.env.VUE_APP_URL);
 
 new Vue({
   router,
